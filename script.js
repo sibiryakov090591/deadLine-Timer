a51e6ef72b1fed0b6a75c53d47ff844dffc9f956
Andrew Sibiryakov <65409658+sibiryakov090591@users.noreply.github.com>
2020-08-03 11:54:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+// Timer
+    const deadLine = '2020-08-11'; <----- set your dead-line in this form!
+
+    function getTimeRemeining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((t / (1000 * 60)) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function addZero (num) { // <----- the function adds 0 to show dates like this: "02" days, "09" hours...
+        if (num >= 0 && num < 10) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setTimer (selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval =  setInterval(updateTimer, 1000);
+
+        updateTimer();
+
+        function updateTimer() {
+            const t = getTimeRemeining(endTime);
+
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setTimer('.timer', deadLine); // <----- first add your "Timer wrapper" selector
